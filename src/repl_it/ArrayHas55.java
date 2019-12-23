e77916b20d2e1b3146ffd9963c743229d66f0f2c
kutluduman <58450274+kutluduman@users.noreply.github.com>
2019-12-23 10:20:54 -0500

    Extracting my Java Programming B15 project

--- src/repl_it/ArrayHas55.java
+++ src/repl_it/ArrayHas55.java
@@ -1,5 +1,6 @@
 package repl_it;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayHas55 {
@@ -7,10 +8,21 @@ public class ArrayHas55 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int[] nums = {input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt(),input.nextInt()};
+        int[] nums = {input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt(), input.nextInt()};
+        boolean output = false;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 5 && nums[i + 1] == 5) {
+
+                output = true;
 
 
+            }
 
+        }
+
+        System.out.println(output);
 
     }
 }
